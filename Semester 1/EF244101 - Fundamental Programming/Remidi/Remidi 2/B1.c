714cb6988989f22937cb67b0857427ff1fc5c815
Shabrina Amalia Safaana <shabrinaamalia860@gmail.com>
2025-07-15 23:22:19 +0700

    Add files via upload

---
+++ Semester 1/EF244101 - Fundamental Programming/Remidi/Remidi 2/B1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#define x 97
+
+int main()
+{
+    char s[1000];
+    gets(s);
+    int len = strlen(s);
+    char modus[26]; // a itu indeksnya 0
+
+    for (int i = 0; i < len; i++){
+        if (s[len] == x + i);
+
+    }
+
+}
