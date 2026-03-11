e232988446a4ee799991c69a53a979cb8ff88d5a
Shabrina Amalia <shabrinaamalia860@gmail.com>
2026-03-11 22:14:44 +0700

    update

--- Semester 4/EF234401 - Network Programming /Tugas/Tugas_1/12-count-vowels.py
+++ Semester 4/EF234401 - Network Programming /Tugas/Tugas_1/12-count-vowels.py
@@ -1,11 +1,10 @@
-vowelsLower = ['a', 'e', 'i', 'o', 'u']
-vowelsUpper = ['A', 'E', 'I', 'O', 'U']
+vowels = "aeiouAEIOU"
 
 s = input()
-while s != 'END':
-    count = 0
-    for i in s:
-        if i in vowelsLower or i in vowelsUpper:
-            count += 1
-    print(count)
-    break
+
+count = 0
+for char in s:
+    if char in vowels:
+        count += 1
+
+print(count)
