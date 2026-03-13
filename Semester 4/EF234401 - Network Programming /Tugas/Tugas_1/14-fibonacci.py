56a9daf218d5597a397dbac750ae072ec99ded24
Shabrina Amalia <shabrinaamalia860@gmail.com>
2026-03-13 19:54:28 +0700

    update

--- Semester 4/EF234401 - Network Programming /Tugas/Tugas_1/14-fibonacci.py
+++ Semester 4/EF234401 - Network Programming /Tugas/Tugas_1/14-fibonacci.py
@@ -1,14 +1,11 @@
 def fib(n):
-    if n <= 0:
-        return "Invalid input"
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
+    if n <= 0: return 0
+    
     a, b = 0, 1
-    for _ in range(2, n):
+    for _ in range(n):
         a, b = b, a + b
-    return b
+    
+    return a
 
 n = int(input())
 print(fib(n))
